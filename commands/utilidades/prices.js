6cdb4fbd7614f15e6fae2e7ee45c40c9818d98cd
EmmanuelGamero <ejgameror@gmail.com>
2022-10-11 16:30:33 -0400

    Inicio del modulo de precios

--- commands/utilidades/prices.js
+++ commands/utilidades/prices.js
@@ -5,8 +5,10 @@ module.exports = {
     options:[],
     run: async (client, int) => {
         try {
-            const response = await axios.get('https://www.albion-online-data.com/api/v2/stats/gold.json?count=1')            
-            console.log(response.data);
+            const response = await axios.get('https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG?locations=Caerleon');
+            const equipment = await axios.get(`https://gameinfo.albiononline.com/api/gameinfo/items/${response.data[0].item_id}/data`);
+            console.log(equipment.data.localizedNames);
+            const image = 'https://render.albiononline.com/v1/item/';
             console.log(response.status);
 
             if (response.status === 200) {
@@ -15,12 +17,45 @@ module.exports = {
                     fetchReply: true
                 }).then((m) =>{
                     const exampleEmbed = {
-                        color: 0x0099ff,
+                        color: 0x0099ff,                        
                         author: {
-                            name: `Precio del Oro: ${response.data[0].price} de Plata`,
-                        }                       
-                    };
-                    m.reply({embeds: [exampleEmbed]});                  
+                            name: `Ciudad: ${response.data[1].city}`,
+                        },
+                        thumbnail: {
+                            url: `${image}/${response.data[0].item_id}.png`,
+                        },
+                        fields: [
+                            {
+                                name: 'Regular field title',
+                                value: 'Some value here',
+                            },
+                            {
+                                name: '\u200b',
+                                value: '\u200b',
+                                inline: false,
+                            },
+                            {
+                                name: 'Inline field title',
+                                value: 'Some value here',
+                                inline: true,
+                            },
+                            {
+                                name: 'Inline field title',
+                                value: 'Some value here',
+                                inline: true,
+                            },
+                            {
+                                name: 'Inline field title',
+                                value: 'Some value here',
+                                inline: true,
+                            },
+                        ],                       
+                        timestamp: new Date().toISOString(),
+                        footer: {
+                            text: 'Consultado',
+                        },
+                    };                    
+                    m.reply({embeds: [exampleEmbed]});            
                 });                  
             }                    
         } catch (error) {
